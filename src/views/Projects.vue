030efaccb6043c75aca28c76a1ff494cf45533f3
CarbonJep <jeffrey.ramos@carbondigital.com.ph>
2019-01-22 18:28:03 +0800

    axios

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="c-projects">
     <div class="o-projects__addProject">
-    	<input type="text" v-model="newProject">
-    	<button @click="addProject">Create Project</button>
+      <input type="text" v-model="newProject">
+      <button @click="addProject">Create Project</button>
     </div>
     <div class="o-projects__projectList o-gridContainer o-grid--3col" >
-    	<Card v-for='(project, index) in projects' :key="index" :cardTitle="project.title" :cardID="index"/>
+      <Card v-for='(project, index) in projects' :key="index" :cardTitle="project.title" :cardID="index"/>
     </div>
   </div>
 </template>
@@ -16,22 +16,21 @@ import Card from '../components/Card.vue';
 export default {
   name: 'Projects',
   data () {
-  	return {
-  		newProject: '',
-  		projects: [
+    return {
+      newProject: '',
+      projects: [
 				{title: 'Project 1'},
 				{title: 'Project 2'},
-  		],
-  	}
+      ],
+    }
   },
   components: {
     Card,
   },
   methods: {
-		addProject(){
+    addProject(){
 			if (this.newProject != '') {
 				this.projects.push({"title": this.newProject});
-				<Project />
         this.newProject='';
 			}else{
 				alert('Please add project title');
